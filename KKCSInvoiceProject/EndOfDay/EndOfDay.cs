3768c1502eea9d18eca958296a97c4127f870423
peter.george.green@gmail.com <Voyger300>
2017-09-03 14:47:51 +1200

    Small fix for the account emails

--- KKCSInvoiceProject/EndOfDay/EndOfDay.cs
+++ KKCSInvoiceProject/EndOfDay/EndOfDay.cs
@@ -86,7 +86,7 @@ namespace KKCSInvoiceProject
 
             // Creates todays date for end of dat
             dtTodaysDate = new DateTime(dtTodaysDate.Year, dtTodaysDate.Month, dtTodaysDate.Day, 12, 0, 0);
-            dtTodaysDate = new DateTime(2017, 8, 30, 12, 0, 0);
+            //dtTodaysDate = new DateTime(2017, 8, 30, 12, 0, 0);
 
             // Creates the title for title
             g_sTitleHeader = dtTodaysDate.Day.ToString() + "/" + dtTodaysDate.Month.ToString() + "/" + dtTodaysDate.Year.ToString();
@@ -264,7 +264,7 @@ namespace KKCSInvoiceProject
 
             DateTime dtDate = DateTime.Today;
             //string query = @"SELECT * FROM Invoice WHERE ReturnMonth = '" + 02 + "' AND ReturnYear = '" + 2017 + "' AND PaidStatus = 'OnAcc' ORDER BY AccountHolder,DateInInvisible DESC";
-            dtDate = new DateTime(2017, 7, dtDate.Day, 12, 0, 0);
+            dtDate = new DateTime(2017, 8, dtDate.Day, 12, 0, 0);
 
             string query = "select * from CustomerInvoices WHERE year(DTReturnDate) = year(@dtDate) AND month(DTDatePaid) = month(@dtDate) AND PaidStatus = 'OnAcc' ORDER BY AccountHolder,DTDateIn ASC";
             command.Parameters.AddWithValue("@dtDate", dtDate);
@@ -286,7 +286,7 @@ namespace KKCSInvoiceProject
             //sCombinedAccount += "Date In" + Padding.Left(5);
 
             //sTitle = "BOI Car Storage Yard - " + sMonthDisplay + " " + sYear + " Accounts";
-            sTitle = "BOI Car Storage Yard - July 2017 Accounts";
+            sTitle = "BOI Car Storage Yard - August 2017 Accounts";
 
             int iPadLength = 25;
 
@@ -378,10 +378,10 @@ namespace KKCSInvoiceProject
                 client.Credentials = new NetworkCredential("pg8472@hotmail.com", "Voyger600!");
                 MailMessage msg = new MailMessage();
                 //msg.To.Add("peter.george.green@gmail.com");
-                msg.To.Add("kerikericarstorage@gmail.com");
-                //msg.To.Add("ar.boiairportcarstorage@outlook.com");
-                //msg.CC.Add("peter.george.green@gmail.com");
-                //msg.CC.Add("deborah.green@hertz.com");
+                //msg.To.Add("kerikericarstorage@gmail.com");
+                msg.To.Add("ar.boiairportcarstorage@outlook.com");
+                msg.CC.Add("peter.george.green@gmail.com");
+                msg.CC.Add("deborah.green@hertz.com");
                 msg.From = new MailAddress("pg8472@hotmail.com");
                 msg.Subject = sTitle;
                 msg.Body = sCombinedAccount;
